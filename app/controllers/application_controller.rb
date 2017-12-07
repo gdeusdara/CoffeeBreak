73189dfd73c8c4959e8f24180fae2a5613c9ec1d
Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>
2017-12-06 22:48:20 -0200

    Adicionada as views da devise para melhor manipulação
    Signed-off-by: Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
- include SessaoHelper
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in) do |usuario_params|
+      usuario_params.permit(:username, :email, :nome)
+    end
+  end
+
+
 end
--- app/helpers/sessao_helper.rb
+++
@@ -1,8 +0,0 @@
-module SessaoHelper
-  def current_user
-      @current_user ||= Usuario.find_by_id(session[:usuario])
-  end
-  def logged_in?
-    current_user != nil
-  end
-end
