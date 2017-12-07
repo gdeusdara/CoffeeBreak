3ba1bc7ceaed84f9fc879654f4d0f83085d417d5
Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>
2017-12-07 12:08:16 -0200

    Adicionado o campo nome. Falta conseguir salvar o que for inserido.
    Signed-off-by: Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 20171205133128) do
   create_table "usuarios", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "nome", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
