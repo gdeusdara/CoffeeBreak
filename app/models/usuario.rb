68120c616b3c4a4d2ef74b6d76da13f454e89c55
Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>
2017-12-10 01:20:31 -0200

    Telas de ver followers e following ja funcionam, o botão de seguir ainda não
    Signed-off-by: Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -5,6 +5,7 @@ class Usuario < ApplicationRecord
                                    foreign_key: "follower_id",
                                    dependent:   :destroy
 
+
   has_many :passive_relationships, class_name:  "Relationship",
                                    foreign_key: "followed_id",
                                    dependent:   :destroy
--- db/schema.rb
+++ db/schema.rb
@@ -47,9 +47,6 @@ ActiveRecord::Schema.define(version: 20171209190544) do
     t.integer "followed_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["followed_id"], name: "index_relationships_on_followed_id"
-    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
-    t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
   create_table "usuarios", force: :cascade do |t|
