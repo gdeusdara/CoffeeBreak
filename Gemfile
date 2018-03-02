0cf2b273b2fd0de16d5244fe126a05b24ed32545
root <root@ip-69-164-194-38.cloudezapp.io>
2018-03-02 00:41:55 -0300

    Configurações para o servidor estabelecidas

--- Gemfile
+++ Gemfile
@@ -12,7 +12,7 @@ gem 'acts_as_votable'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.4'
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+#gem 'sqlite3'
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
 # Use SCSS for stylesheets
@@ -66,4 +66,6 @@ gem "paperclip", "~> 5.0.0"
 
 gem 'rails_admin', '~> 1.3'
 
-gem 'will_paginate-bootstrap'
+gem 'unicorn'
+
+gem 'mysql2'
--- config/application.rb
+++ config/application.rb
@@ -10,11 +10,10 @@ module CoffeeBreak
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-config.time_zone = 'Brasilia'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile =true 
   end
 end
